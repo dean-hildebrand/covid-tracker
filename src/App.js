87a1187310a0de150697fc7beb36a2e65a4729ba
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-19 11:00:18 -0500

    cleaned up react app and commented components for wireframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+    <h1>Covid Tracker</h1>
+
+    {/* Header */}
+    {/* Title + Selected input dropdown */}
+
+    {/* Info Boxes */}
+    {/* Info Boxes */}
+    {/* Info Boxes */}
+
+    {/* Table */}
+    {/* Graph */}
+
+    {/* Map */}
     </div>
   );
 }
