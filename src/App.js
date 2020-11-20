1b5787eb3bb2b88483df570931e0b2836ccf7d7d
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-20 11:27:02 -0500

    InfoBox component created and made Card with material-ui

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { FormControl, MenuItem, Select } from "@material-ui/core";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState(["worldwide"]);
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -15,32 +16,37 @@ function App() {
             value: country.countryInfo.iso2,
           }));
 
-          setCountries(countries)
+          setCountries(countries);
         });
     };
     getCountriesData();
   }, []);
+
+  const onCountryChange = (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
 
   return (
     <div className="app">
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
-          {countries.map((country) => (
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-          ))}
-
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
           </Select>
         </FormControl>
       </div>
 
-      {/* Header */}
-      {/* Title + Selected input dropdown */}
+      <div className="app__stats">
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      </div>
 
-      {/* Info Boxes */}
-      {/* Info Boxes */}
-      {/* Info Boxes */}
 
       {/* Table */}
       {/* Graph */}
