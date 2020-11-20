19e62d3d9f259ba669aec287564ef98c86b54618
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-20 11:11:29 -0500

    useEffect to grab all the countries by name and put into menuItem dropdown

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import React from 'react'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { FormControl, MenuItem, Select } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries)
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-    <h1>Covid Tracker</h1>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+          {countries.map((country) => (
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
 
-    {/* Header */}
-    {/* Title + Selected input dropdown */}
+          </Select>
+        </FormControl>
+      </div>
 
-    {/* Info Boxes */}
-    {/* Info Boxes */}
-    {/* Info Boxes */}
+      {/* Header */}
+      {/* Title + Selected input dropdown */}
 
-    {/* Table */}
-    {/* Graph */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
+      {/* Info Boxes */}
 
-    {/* Map */}
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
