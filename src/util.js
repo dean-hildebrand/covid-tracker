f04573b43eb9103cb0b93df808ca7591c45b2218
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-24 18:59:18 -0500

    Added circles as popup depending on number of cases

--- src/util.js
+++ src/util.js
@@ -8,20 +8,20 @@ import { Circle, Popup } from 'react-leaflet'
 const casesTypeColors ={
   cases: {
     hex: '#CC1034',
-    rgb: "rgb(204, 16, 52)",
-    half_op: "rgba(204, 16, 52, 0.5)",
+    //rgb: "rgb(204, 16, 52)",
+    //half_op: "rgba(204, 16, 52, 0.5)",
     multiplier: 800,
   },
   recovered: {
     hex: '#7dd71d',
-    rgb: "rgb(125, 215, 29)",
-    half_op: "rgba(125, 215, 29, 0.5)",
+    //rgb: "rgb(125, 215, 29)",
+    //half_op: "rgba(125, 215, 29, 0.5)",
     multiplier: 1200,
   },
-  cases: {
+  deaths: {
     hex: '#fb4443',
-    rgb: "rgb(251, 68, 67)",
-    half_op: "rgba(251, 68, 67, 0.5)",
+    //rgb: "rgb(251, 68, 67)",
+    //half_op: "rgba(251, 68, 67, 0.5)",
     multiplier: 2000,
   },
 }
@@ -44,12 +44,19 @@ export const sortData = (data) => {
 
 
 //Draw circles on map and display size based on cases
-export const showDataOnMap = (data, casesType="cases") => (
+export const showDataOnMap = (data, casesType="cases") =>
   data.map(country => (
     <Circle
     center={[country.countryInfo.lat, country.countryInfo.long]}
     fillOpacity={0.4}
+    color={casesTypeColors[casesType].hex}
+    fillColor={casesTypeColors[casesType].hex}
+    radius={
+      Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+    }
     >
+    <Popup>
+    <h1>IM A POPUP</h1>
+    </Popup>
     </Circle>
   ))
-);
