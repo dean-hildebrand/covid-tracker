b403922c2117c5e3cfd41fa23914669a21342897
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-23 19:36:05 -0500

    Color objects created for different case types

--- src/util.js
+++ src/util.js
@@ -1,5 +1,32 @@
-//sorted data of countries by cases of covid 19
+import React from 'react';
+import numeral from 'numeral';
+import { Circle, Popup } from 'react-leaflet'
 
+
+//Circle style based on deaths, recovered, or cases
+
+const casesTypeColors ={
+  cases: {
+    hex: '#CC1034',
+    rgb: "rgb(204, 16, 52)",
+    half_op: "rgba(204, 16, 52, 0.5)",
+    multiplier: 800,
+  },
+  recovered: {
+    hex: '#7dd71d',
+    rgb: "rgb(125, 215, 29)",
+    half_op: "rgba(125, 215, 29, 0.5)",
+    multiplier: 1200,
+  },
+  cases: {
+    hex: '#fb4443',
+    rgb: "rgb(251, 68, 67)",
+    half_op: "rgba(251, 68, 67, 0.5)",
+    multiplier: 2000,
+  },
+}
+
+//sorted data of countries by cases of covid 19
 export const sortData = (data) => {
   const sortedData = [...data];
 
@@ -10,8 +37,19 @@ export const sortData = (data) => {
       return 1;
     }
   });
+  //one liner
+  //return sortedData.sort(a,b) => (a.case > b.cases ? -1 : 1)
   return sortedData;
 };
 
-//one liner
-//return sortedData.sort(a,b) => (a.case > b.cases ? -1 : 1)
+
+//Draw circles on map and display size based on cases
+export const showDataOnMap = (data, casesType="cases") => (
+  data.map(country => (
+    <Circle
+    center={[country.countryInfo.lat, country.countryInfo.long]}
+    fillOpacity={0.4}
+    >
+    </Circle>
+  ))
+);
