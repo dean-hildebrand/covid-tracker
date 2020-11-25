e37fc1e5c403641661844b77b470bdce0dc29e0a
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-24 19:10:20 -0500

    Added flag to info popup

--- src/util.js
+++ src/util.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import numeral from 'numeral';
-import { Circle, Popup } from 'react-leaflet'
-
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
 
 //Circle style based on deaths, recovered, or cases
 
-const casesTypeColors ={
+const casesTypeColors = {
   cases: {
-    hex: '#CC1034',
+    hex: "#CC1034",
     //rgb: "rgb(204, 16, 52)",
     //half_op: "rgba(204, 16, 52, 0.5)",
     multiplier: 800,
   },
   recovered: {
-    hex: '#7dd71d',
+    hex: "#7dd71d",
     //rgb: "rgb(125, 215, 29)",
     //half_op: "rgba(125, 215, 29, 0.5)",
     multiplier: 1200,
   },
   deaths: {
-    hex: '#fb4443',
+    hex: "#fb4443",
     //rgb: "rgb(251, 68, 67)",
     //half_op: "rgba(251, 68, 67, 0.5)",
     multiplier: 2000,
   },
-}
+};
 
 //sorted data of countries by cases of covid 19
 export const sortData = (data) => {
@@ -42,29 +41,35 @@ export const sortData = (data) => {
   return sortedData;
 };
 
-
 //Draw circles on map and display size based on cases
-export const showDataOnMap = (data, casesType="cases") =>
-  data.map(country => (
+export const showDataOnMap = (data, casesType = "cases") =>
+  data.map((country) => (
     <Circle
-    center={[country.countryInfo.lat, country.countryInfo.long]}
-    fillOpacity={0.4}
-    color={casesTypeColors[casesType].hex}
-    fillColor={casesTypeColors[casesType].hex}
-    radius={
-      Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-    }
+      center={[country.countryInfo.lat, country.countryInfo.long]}
+      fillOpacity={0.4}
+      color={casesTypeColors[casesType].hex}
+      fillColor={casesTypeColors[casesType].hex}
+      radius={
+        Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+      }
     >
-    <Popup>
-    <div>
-    <div
-    style={{ backgroundImage: `url(${country.countryInfo.flag})`}}
-    />
-    <div>{country.country}</div>
-    <div>Cases: {numeral(country.cases).format("0,0")}</div>
-    <div>Recovered: {numeral(country.recovered).format("0,0")}:</div>
-    <div>Deaths: {numeral(country.deaths).format("0,0")}</div>
-    </div>
-    </Popup>
+      <Popup>
+        <div className="infoContainer">
+          <div
+            className="info-flag"
+            style={{ backgroundImage: `url(${country.countryInfo.flag})` }}
+          />
+          <div className="info-name">{country.country}</div>
+          <div className="info-confirmed">
+            Cases: {numeral(country.cases).format("0,0")}
+          </div>
+          <div className="info-recovered">
+            Recovered: {numeral(country.recovered).format("0,0")}:
+          </div>
+          <div className="info-deaths">
+            Deaths: {numeral(country.deaths).format("0,0")}
+          </div>
+        </div>
+      </Popup>
     </Circle>
-  ))
+  ));
